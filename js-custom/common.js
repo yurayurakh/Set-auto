163a060e462e3a528154d1abe6ceda30d616d924
Yurii Yurakh <yurayurakh@gmail.com>
2017-09-30 17:10:06 +0300

    validate-form

--- js-custom/common.js
+++ js-custom/common.js
@@ -8,4 +8,43 @@ $(document).ready(function() {
         return false;
     });
 
+    // Onfocus input
+    $('.form__item_input').focusout(function(){
+        if ($(this).val().length == 0) {
+            $(this).next().removeClass("input-no-empty");
+        } else if ($(this).val() == "+7(___) ___-____") {
+            $(this).next().removeClass("input-no-empty");
+        }
+        else {
+            $(this).next().addClass("input-no-empty");
+        }
+
+
+    });
+
+    <!-- Phone mask -->
+    $(".tel").mask("+7(999) 999-9999");
+    
+    
+    //Tabs 
+    $('.service__tab_item').click(function () {
+        $('.service__tab_item').removeClass("tab-active");
+        $(this).addClass("tab-active");
+        var tab = $(this).data("tab");
+        $(".tab-content").removeClass("tab-content-active");
+        jQuery.each($(".tab-content"),function(i,val){
+            if (val.id == tab ) {
+                $(val).addClass("tab-content-active");
+            }
+        });
+    })
+
+    //Points Car
+    $('.point_item-label').click(function () {
+        if($(this).next().css("display") == "none" ) {
+            $(this).next().fadeIn("slow");
+        } else {
+            $(this).next().fadeOut("slow");
+        }
+    });
 });
---
+++ js-custom/valide-form.js
@@ -0,0 +1,78 @@
+(function(){
+
+    var app = {
+
+        initialize : function () {
+            this.setUpListeners();
+        },
+
+        setUpListeners : function () {
+            $('form').on('submit', app.submitForm);
+            $('form').on('keydown', 'input', app.removeError);
+        },
+
+        submitForm : function (e) {
+            e.preventDefault();
+
+            var form = $(this),
+                submitBtn = form.find('button');
+
+            if ( app.validateForm(form) === false ) return false;
+
+            submitBtn.attr('disabled', 'disabled');
+
+            var str = form.serialize();
+
+            $.ajax ({
+                url: 'contact_form/contact_process.php',
+                type: 'POST',
+                data: str
+            })
+                .done(function(msg){
+                    if(msg === "OK"){
+                        setTimeout(function(){
+                            $('.close-modal').click();
+                        }, 150);
+                        setTimeout(function(){
+                            $('.success-modal').click();
+                        }, 500);
+                        setTimeout(function(){
+                            $('.close-modal').click();
+                        }, 3000);
+                    } else {
+                        form.html(msg);
+                    }
+                })
+                .always(function(){
+                    submitBtn.removeAttr('disabled');
+                });
+        },
+
+        validateForm : function (form) {
+            var inputs = form.find('input'),
+                valid = true;
+
+            $.each(inputs, function(index, val) {
+                var input = $(val),
+                    val = input.val(),
+                    formGroup = input;
+
+                if (val.length === 0) {
+                    formGroup.addClass('has-error').removeClass('has-success');
+                    valid = false;
+                }else {
+                    formGroup.addClass('has-success').removeClass('has-error');
+                }
+            });
+
+            return valid;
+        },
+
+        removeError : function () {
+            $(this).removeClass('has-error')
+        }
+    };
+
+    app.initialize();
+
+}());
